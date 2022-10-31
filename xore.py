4a27f7aeedf2efcb79af5c92ec6f7e371a29d0a7
Repinoid <96376250+Repinoid@users.noreply.github.com>
2022-10-31 20:46:49 +0300

    Update xore.py

--- xore.py
+++ xore.py
@@ -4,33 +4,36 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
 
-cats = os.listdir("C:/WORK/DL0/vmesto/Cat")
-dogs = os.listdir("C:/WORK/DL0/vmesto/Dog")
+dl_folder = "C:/WORK/DL0/"
+cats_dir = dl_folder + "vmesto/Cat/"
+dogs_dir = dl_folder + "vmesto/Dog/"
 
+cats = os.listdir(cats_dir)
+dogs = os.listdir(dogs_dir)
 
 def get_masked (orig, leva = 1, shift_y = 1, shift_x = 1):
-    orig = orig.copy()
-    hei = len(orig)
-    wid = len(orig[0])
-    for y in range(hei - shift_y - 1):
-        for x in range(wid - shift_x - 1):
-            orig[y, x] = (orig[y, x] ^ orig[y + shift_y, x + shift_x] // leva) & 0xFF
+    orig = orig.copy()                      # make a copy because original is read-only
+    hei = len(orig)                         # image height, pixels
+    wid = len(orig[0])                      # width
+    for y in range(hei - shift_y):          # shift_y - vertical shift of masking image
+        for x in range(wid - shift_x):      # shift_x - horizontal shift of masking image
+            orig[y, x] = ( orig[y, x] ^ (orig[y + shift_y, x + shift_x] // leva) ) & 0xFF   # XORing
+                                            # leva - denominator of masking pixel RGB values
     return orig
 
 
 for ind in range(len(cats)):
     print("Cat ", ind)
-    ori = mpimg.imread("C:/WORK/DL0/vmesto/Cat/" + cats[ind])
-    shit = get_masked(ori, 2)
-    im = Image.fromarray(shit)
-    im.save("C:/WORK/DL0/mask2/Cat/"+cats[ind])
+    ori = mpimg.imread(cats_dir + cats[ind])        # read pet image
+    shift = get_masked(ori, 2)                      # create masked image, mask denominator here - 2
+    im = Image.fromarray(shift)
+    im.save(dl_folder + "mask20/Cat/" +cats[ind])    # save
 
 for ind in range(len(dogs)):
     print("Dog ", ind)
-    ori = mpimg.imread("C:/WORK/DL0/vmesto/Dog/" + dogs[ind])
+    ori = mpimg.imread(dogs_dir + dogs[ind])
     shit = get_masked(ori, 2)
     im = Image.fromarray(shit)
-    im.save("C:/WORK/DL0/mask2/Dog/"+dogs[ind])
+    im.save(dl_folder + "mask20/Dog/" + dogs[ind])
 
-#imgplot = plt.imshow(shit)
-#plt.show()
+
